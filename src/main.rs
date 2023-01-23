a872491f00835bd9060dff9cfbeca059f669ef4a
Brian Logan <blogan@blogan-home>
2023-01-22 22:10:04 -0500

    Checkpoint for reflection

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ struct World {
     name_components: Vec<Option<Name>>,
 }
 
+
 impl World {
 
     fn new() -> Self {
@@ -15,11 +16,37 @@ impl World {
         }
     }
 
+    fn new_entity(&mut self, health: Option<Health>, name: Option<Name>) {
+        self.health_components.push(health);
+        self.name_components.push(name);
+    }
+
 
 }
 
 
 
 fn main() {
-    println!("Hello, world!");
+    let mut world = World::new();
+    world.new_entity(Some(Health(-10)), Some(Name("Icarus")));
+    world.new_entity(Some(Health(100)), Some(Name("Prometheus")));
+    world.new_entity(None, Some(Name("Zeus")));
+
+    let zip = world
+        .health_components
+        .iter()
+        .zip(world.name_components.iter());
+    
+    let with_health_and_name = 
+        zip.filter_map(|(health,name): (&Option<Health>, &Option<Name>)| {
+            Some((health.as_ref()?, name.as_ref()?))
+        });
+
+    for (health, name) in with_health_and_name {
+        if health.0 < 0 {
+            println!("{} has perished!", name.0);
+        } else {
+            println!("{} is still healthy", name.0);
+        }
+    }
 }
