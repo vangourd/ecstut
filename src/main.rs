e4b74b3e193fc33163dbaef4712a745d63e47922
Brian Logan <mail@brianjlogan.com>
2023-01-27 12:24:28 -0500

    latest progress

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,51 @@
 struct Health(i32);
 struct Name(&'static str);
 
+trait ComponentVec {
+    fn push_none(&mut self);
+}
+
 struct World {
-    health_components: Vec<Option<Health>>,
-    name_components: Vec<Option<Name>>,
+    entities_count: usize,
+    component_vecs: Vec<Box<dyn ComponentVec>>,
+}
+
+impl<T> ComponentVec for Vec<Option<T>> {
+    fn push_none(&mut self) {
+        self.push(None)
+    }
 }
 
 impl World {
 
     fn new() -> Self {
         Self {
-            health_components: Vec::new(),
-            name_components:Vec::new(),
+            entities_count: 0,
+            component_vecs: Vec::new()
         }
     }
 
+    fn new_entity(&mut self) -> usize { 
+        let entity_id = self.entities_count;
+        for component_vec in self.component_vecs.iter_mut() {
+            component_vec.push_none();
+        }
+        self.entities_count += 1;
+        entity_id
+    }
+
+    fn add_component_to_entity<ComponentType: 'static>(
+        &mut self,
+        entity: usize,
+        component: ComponentType,
+    ){
+        /* do stuff  */
+    }
 
 }
 
 
 
 fn main() {
-    println!("Hello, world!");
+    
 }
