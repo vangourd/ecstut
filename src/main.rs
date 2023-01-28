42655440528fb37d435e2fa4b2d30d122ae8c8e7
Brian Logan <blogan@blogan-home>
2023-01-28 09:52:06 -0500

    First we iterate through all component_vecs

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,33 @@ struct Health(i32);
 struct Name(&'static str);
 
 trait ComponentVec {
+    fn as_any(&self) -> &dyn std::any::Any;
+    fn as_any_mut(&mut self) -> &mut dyn std::any::Any;
     fn push_none(&mut self);
 }
+
+
+impl<T: 'static> ComponentVec for Vec<Option<T>> {
+
+    fn as_any(&self) -> &dyn std::any::Any {
+        self as &dyn std::any::Any
+    }
+
+    fn as_any_mut(&mut self) -> &mut dyn std::any::Any {
+        self as &mut dyn std::any::Any
+    }
+
+    fn push_none(&mut self) {
+        self.push(None)
+    }
+
+
+}
+
 
 struct World {
     entities_count: usize,
     component_vecs: Vec<Box<dyn ComponentVec>>,
-}
-
-impl<T> ComponentVec for Vec<Option<T>> {
-    fn push_none(&mut self) {
-        self.push(None)
-    }
 }
 
 
@@ -26,9 +41,37 @@ impl World {
         }
     }
 
-    fn new_entity(&mut self, health: Option<Health>, name: Option<Name>) {
-        self.health_components.push(health);
-        self.name_components.push(name);
+    fn new_entity(&mut self) -> usize {
+        let entity_id = self.entities_count;
+        for component_vec in self.component_vecs.iter_mut() {
+            component_vec.push_none();
+        }
+        self.entities_count += 1;
+        entity_id
+    }
+
+    fn add_component_to_entity<ComponentType: 'static>(
+        &mut self,
+        entity: usize,
+        component: ComponentType,
+    ) {
+        for component_vec in self.component_vecs.iter_mut() {
+            if let Some(component_vec) = component_vec
+                .as_any_mut()
+                .downcast_mut::<Vec<Option<ComponentType>>>() {
+                    component_vec[entity] = Some(component);
+                    return;
+                }
+        }
+        let mut new_component_vec: Vec<Option<ComponentType>> = 
+            Vec::with_capacity(self.entities_count);
+        
+        for _ in 0..self.entities_count {
+            new_component_vec.push(None);
+        }
+
+        new_component_vec[entity] = Some(component);
+        self.component_vecs.push(Box::new(new_component_vec));
     }
 
 
